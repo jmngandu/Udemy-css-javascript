54f8c33f4df33da3e5de76a396245fae389a2ad7
James Ngandu <jamesmngandu@gmail.com>
2023-06-01 05:11:57 +0300

    Js

--- javascript/main.js
+++ javascript/main.js
@@ -7,13 +7,4 @@ while(j < 10)
 {
 ++j;
 }
-console.log(j);
 
-const FirstName = "James";
-const LastName  = "Ngandu";
-
-const sentence = "Hello "+FirstName+" "+LastName+"How are you?!"
-console.log(sentence);
-const jname = "Mukuvi";
-const description = `His name is ${jname} He is 20yrs`;
-console.log(description);
