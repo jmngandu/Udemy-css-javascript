9f8a9bd55c633d0922704a2a04f1359e0bc2eeaf
James Ngandu <jamesmngandu@gmail.com>
2023-06-01 05:50:38 +0300

    Js

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,4 @@
-
-for(m = 1; m<11; m++)
-console.log(`forloop: ${m}`);
-
-j = 1;
-while(j < 10)
-{
-++j;
-}
-
-const SkyBlue = true;
+const SkyBlue = false;
 if (SkyBlue){
 console.log("The sky is blue");
 }
@@ -16,3 +6,20 @@ else{
     console.log("The sky is not blue");
 }
 
+if (2+2 === 4){
+    console.log("Correct!");
+}else
+{
+    console.log("Wrong");
+}
+
+const YourAge = 18;
+if (YourAge === 5){
+    console.log("Your young my friend");
+}else if (YourAge >= 10){
+    console.log("Go back home");
+}else if (YourAge === 18){
+    console.log("Eligible to vote");
+}else if (YourAge >= 18){
+    console.log("Your too old my friend");
+}
